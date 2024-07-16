3c210193af3887bc92f5d5d5f9c72e76d8f65029
bobvanboekel <33581442+bobvanboekel@users.noreply.github.com>
2024-07-16 22:54:05 +0200

    Add folder, vertical center body, fix dark mode
    Added the 'Ontwerpen' folder. Vertically centered the body and removed the welcome message, fixed colors for dark mode.

--- js.js
+++ js.js
@@ -8,28 +8,8 @@ function updateDateTime() {
     dateTimeElement.textContent = formattedDateTime;
 }
 
-function timeOfDay() {
-    const timeOfDayElement = document.getElementById("timeOfDay");
-    const hour = new Date().getHours();
-    var timeOfDay;
-    if (hour >= 18) {
-        timeOfDay = "navond";
-    }
-    else if (hour >= 12) {
-        timeOfDay = "middag";
-    }
-    else if (hour >= 6) {
-        timeOfDay = "morgen";
-    }
-    else {
-        timeOfDay = "nacht";
-    }
-    timeOfDayElement.textContent = timeOfDay
-}
-
 // Update the date and time immediately when the page loads
 updateDateTime();
-timeOfDay();
 
 // Update the date and time every second
 setInterval(updateDateTime, 1000);
